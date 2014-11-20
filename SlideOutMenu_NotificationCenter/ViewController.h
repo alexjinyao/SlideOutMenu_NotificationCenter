d2cc1b9b9ac79584cc8ea39e41dc4429f71ba80a
alexjinyao <alexjinyao@gmail.com>
2014-11-20 12:35:24 -0500

    Initial Commit

---
+++ SlideOutMenu_NotificationCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideOutMenu_NotificationCenter
+//
+//  Created by Jin on 11/20/14.
+//  Copyright (c) 2014 Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
